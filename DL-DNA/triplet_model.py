e3c9337f01e577ca8d888a3f6e06e658125437c3
KyungWoon Cho <cezanne@codemayo.com>
2024-08-21 19:52:35 +0900

    apply triplet loss with 1 batch size
    merge chanhee's works which are advantageous to dl-dna
    - use tensorflow instead of keras.backend
    - define custom object

--- DL-DNA/triplet_model.py
+++ DL-DNA/triplet_model.py
@@ -1,9 +1,10 @@
-from keras.applications import MobileNet
-from keras.layers import Dense
-from keras.models import Model
-import keras.backend as backend
-import keras.saving
-from keras.models import load_model
+import tensorflow as tf
+# noinspection PyUnresolvedReferences
+from tensorflow.keras.applications import MobileNet
+# noinspection PyUnresolvedReferences
+from tensorflow.keras.layers import Dense
+# noinspection PyUnresolvedReferences
+from tensorflow.keras.models import Model, load_model
 
 import numpy as np
 
@@ -11,20 +12,18 @@ import dl_dna_model
 from lineEnumerator import LineEnumerator
 
 
-@keras.saving.register_keras_serializable()
-# Triplet Loss
-def _triplet_loss(y_true, y_pred, alpha=0.4):
+def _triplet_loss(_y_true, y_pred, alpha=0.4):
     anchor = y_pred[0]
     positive = y_pred[1]
     negative = y_pred[2]
 
     # 거리 계산
-    pos_dist = backend.sum(backend.square(anchor - positive))
-    neg_dist = backend.sum(backend.square(anchor - negative))
+    pos_dist = tf.reduce_sum(tf.square(anchor - positive))
+    neg_dist = tf.reduce_sum(tf.square(anchor - negative))
 
     # Triplet Loss
     basic_loss = pos_dist - neg_dist + alpha
-    loss = backend.maximum(basic_loss, 0.0)
+    loss = tf.maximum(basic_loss, 0.0)
 
     return loss
 
@@ -33,27 +32,26 @@ class ModelTriplet(dl_dna_model.DlDnaModel):
     def __init__(self):
         super().__init__()
 
-        self.dl_model = MobileNet(weights='imagenet', include_top=False, pooling='avg')
+        tf.config.run_functions_eagerly(True)
+        mobilenet = MobileNet(weights='imagenet', include_top=False, pooling='avg')
 
-        embedding_layer = Dense(dl_dna_model.n_units, activation='relu')(self.dl_model.output)
-        self.dl_model = Model(inputs=self.dl_model.input, outputs=embedding_layer)
-        self.dl_model.compile(optimizer='adam', loss=_triplet_loss)
+        embedding_layer = Dense(dl_dna_model.n_units, activation='relu')(mobilenet.output)
+        self.dl_model = Model(inputs=mobilenet.input, outputs=embedding_layer)
+        self.dl_model.compile(optimizer='adam', loss=_triplet_loss, metrics=['accuracy'])
         self.verbose_level = 1 if dl_dna_model.verbose else 0
 
     @staticmethod
-    def _load_triplet(triplet):
+    def _load_triplets(triples):
         images = []
-        for name in triplet:
-            images.append(dl_dna_model.load_img_data(name))
+        for triplet in triples:
+            for name in triplet:
+                images.append(dl_dna_model.load_img_data(name))
         return np.array(images)
 
     def _train_triplet_model(self, triples):
-        dummy_y = np.array([1, 1, 1])
-        verbose_level = 1 if dl_dna_model.verbose else 0
-
-        for triple in triples:
-            images = self._load_triplet(triple)
-            self.dl_model.fit(x=images, y=dummy_y, epochs=dl_dna_model.epochs, verbose=verbose_level)
+        images = self._load_triplets(triples)
+        dummy_y = np.ones((images.shape[0],))
+        self.dl_model.fit(x=images, y=dummy_y, batch_size=3, epochs=dl_dna_model.epochs, verbose=self.verbose_level)
 
     def train(self, fpath_train: str):
         triples = LineEnumerator(fpath_train, True)
@@ -66,4 +64,4 @@ class ModelTriplet(dl_dna_model.DlDnaModel):
         self.dl_model.save(path_save)
 
     def load(self, path_load: str):
-        self.dl_model = load_model(path_load)
+        self.dl_model = load_model(path_load, custom_objects={'_triplet_loss': _triplet_loss})
