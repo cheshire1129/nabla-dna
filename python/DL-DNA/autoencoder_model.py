ec6d3b1aff36993668788a4c3f8151961e57397a
KyungWoon Cho <cezanne@codemayo.com>
2024-11-04 16:32:59 +0900

    Fixed autoencoder performance issue
    The autoencoder model was experiencing performance problems,
    potentially due to dead neurons.
    To address this, the model was refactored based on
    the PyImageSearch autoencoder tutorial.
    The specific tutorial link has been added as a code comment for traceability.

--- python/DL-DNA/autoencoder_model.py
+++ python/DL-DNA/autoencoder_model.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
 # noinspection PyUnresolvedReferences
-from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, Reshape, MaxPooling2D, UpSampling2D, BatchNormalization
+from tensorflow.keras.layers import Input, Dense, Conv2D, Conv2DTranspose, Flatten, Reshape, LeakyReLU, BatchNormalization
 # noinspection PyUnresolvedReferences
 from tensorflow.keras.models import Model, load_model
 # noinspection PyUnresolvedReferences
 from tensorflow.keras.optimizers import Adam
 # noinspection PyUnresolvedReferences
 from tensorflow.keras.callbacks import EarlyStopping
+# noinspection PyUnresolvedReferences
+from tensorflow.keras import backend as K
+
 from scipy import spatial
 
 import numpy as np
@@ -18,40 +21,37 @@ from lineEnumerator import LineEnumerator
 full_batch = False
 path_decoded_image = None
 
+# from https://pyimagesearch.com/2020/02/17/autoencoders-with-keras-tensorflow-and-deep-learning/
+
 class AutoEncoder(dl_dna_model.DlDnaModel):
     def __init__(self):
         super().__init__()
 
+        filters = (64, 32)
+
         tf.config.run_functions_eagerly(True)
 
-        input_layer = Input(shape=(224, 224, 3))
+        x = input_layer = Input(shape=(224, 224, 3))
 
-        x = Conv2D(64, (3, 3), activation='relu', padding='same')(input_layer)
-        x = MaxPooling2D((2, 2), padding='same')(x)
+        for f in filters:
+            x = Conv2D(f, (3, 3), strides=2, padding="same")(x)
+            x = LeakyReLU(alpha=0.2)(x)
+            x = BatchNormalization()(x)
 
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-        x = MaxPooling2D((2, 2), padding='same')(x)
-
-        x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-        x = MaxPooling2D((2, 2), padding='same')(x)
-
-        volume_size = np.prod(x.shape[1:])
+        volume_size = K.int_shape(x)
         x = Flatten()(x)
-        encoded = Dense(dl_dna_model.n_units, activation='relu', name='encoded')(x)
 
-        x = Dense(volume_size, activation='relu')(encoded)
-        x = Reshape((28, 28, 16))(x)
+        encoded = Dense(dl_dna_model.n_units, name='encoded')(x)
 
-        x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((2, 2))(x)
+        x = Dense(np.prod(volume_size[1:]))(encoded)
+        x = Reshape((volume_size[1], volume_size[2], volume_size[3]))(x)
 
-        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((2, 2))(x)
+        for f in filters[::-1]:
+            x = Conv2DTranspose(f, (3, 3), strides=2, padding="same")(x)
+            x = LeakyReLU(alpha=0.2)(x)
+            x = BatchNormalization()(x)
 
-        x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-        x = UpSampling2D((2, 2))(x)
-
-        decoded = Conv2D(3, (3, 3), activation='relu', padding='same')(x)
+        decoded = Conv2DTranspose(3, (3, 3), activation='sigmoid', padding="same")(x)
 
         self.dl_model = Model(input_layer, decoded)
         optimizer = Adam(learning_rate=0.001)
@@ -72,6 +72,8 @@ class AutoEncoder(dl_dna_model.DlDnaModel):
         lines = LineEnumerator(fpath_train)
         images = self._load_images(lines)
 
+        images = images / 255.
+
         batch_size = dl_dna_model.batch_size if dl_dna_model.batch_size != 0 else len(images)
         self.dl_model.fit(images, images, epochs=dl_dna_model.epochs, batch_size=batch_size,
                           verbose=self.verbose_level)
@@ -79,12 +81,13 @@ class AutoEncoder(dl_dna_model.DlDnaModel):
     def extract_dna(self, data):
         global path_decoded_image
 
+        data = data / 255.
         if path_decoded_image:
-            img_load.save_img(path_decoded_image, self.dl_model.predict(data)[0])
+            img_load.save_img(path_decoded_image, self.dl_model.predict(data)[0] * 255.)
         return self.encoder.predict(data, verbose=self.verbose_level)[0]
 
     def _get_distance(self, dna1, dna2):
-        return 2 * spatial.distance.cosine(dna1, dna2)
+        return spatial.distance.cosine(dna1, dna2)
 
     def save(self, path_save: str):
         self.dl_model.save(path_save)
